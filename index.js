4696f3994f7b3a026a5f649f83441d89833aa8c8
home <long.le@gs25.com.vn>
2024-09-16 13:55:24 +0700

    feat: health check

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+app.get('/alive', (req, res) => {
+    return "Alive"
+})
+
 app.get('/', (req, res) => {
     const file = `${__dirname}/TVAdsAgentV6.apk`;
     res.download(file); // Set disposition and send it.
