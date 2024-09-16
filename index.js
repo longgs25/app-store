115aba5d1df24e96a9b50839eb4d8cdaf23e7f98
home <long.le@gs25.com.vn>
2024-09-16 14:13:20 +0700

    feat: html

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get('/alive', (req, res) => {
     return "Alive"
 })
 
+app.get('/page',(req,res)=>{
+    return "<html> <h1>hihihi</h1> </html>"
+})
+
 app.get('/', (req, res) => {
     const file = `${__dirname}/TVAdsAgentV6.apk`;
     res.download(file); // Set disposition and send it.
